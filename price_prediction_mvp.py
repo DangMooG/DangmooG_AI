57031d3e16cb8d74d650f29dc98d1074f28a4903
Kangbeen Ko <sweetrainforyou@gmail.com>
2023-09-17 18:41:07 +0900

    feat: alpha value based on brisque score

--- price_prediction_mvp.py
+++ price_prediction_mvp.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import PIL.Image
+from brisque import BRISQUE
+
+obj = BRISQUE(url=False)
+
+path = '/Users/kangbeenko/Desktop/GitHub_Repository/dangmoog-ai/original-scaled-image.jpg'
+img = PIL.Image.open(path)
+
+
 
 product = '농구공'
+alpha = round(obj.score(img)/100, 3)
 
 # 웹 페이지의 URL 설정
 url = f'https://web.joongna.com/search-price/{product}?'
@@ -12,14 +22,20 @@ response = requests.post(url)
 # 응답 받은 HTML을 파싱
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# 시세금액을 포함하는 div 요소를 찾기
-div_with_price = soup.find('div', class_='graphTitle')
+mean_price_div = soup.find('div', class_='graphTitle')
+lower_price_span = soup.find('span', {'class': 'text-blue-500'})
 
-# 시세금액 텍스트 추출
-price = div_with_price.find('p').text.strip()
-numbers = re.findall(r'\d+', price)
+mean_price = mean_price_div.find('p').text.strip()
+lower_price = lower_price_span.find('span', {'class': 'font-bold'}).text.strip()
 
-# 추출된 숫자를 하나의 문자열로 합치고 정수로 변환
-result = int(''.join(numbers))
+mean_price = re.findall(r'\d+', mean_price)
+lower_price = re.findall(r'\d+', lower_price)
+
+mean_price = int(''.join(mean_price))
+lower_price = int(''.join(lower_price))
+
+# price algorithm - will be modified
+result = (1-alpha) * mean_price + alpha * lower_price
+result = int(round(result, -1))
 
 print(result)
