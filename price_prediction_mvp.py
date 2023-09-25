11159e962c00c87e8355cf7d381f828b49a7e323
Kangbeen Ko <sweetrainforyou@gmail.com>
2023-09-25 11:59:50 +0900

    refactor: refactor the whole process

--- price_prediction_mvp.py
+++ price_prediction_mvp.py
@@ -4,38 +4,50 @@ import re
 import PIL.Image
 from brisque import BRISQUE
 
-obj = BRISQUE(url=False)
+def get_brisque_score(img_path):
+    obj = BRISQUE(url=False)
+    alpha = round(obj.score(img)/100, 3)
+    return alpha
 
-path = '/Users/kangbeenko/Desktop/GitHub_Repository/dangmoog-ai/original-scaled-image.jpg'
-img = PIL.Image.open(path)
+def scrapp_joogna(product_name):
+    url = f'https://web.joongna.com/search-price/{product_name}?'
 
+    response = requests.post(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        trend_price_div = soup.find('div', class_='graphTitle')
+        lower_price_span = soup.find('span', {'class': 'text-blue-500'})    
 
+        trend_price = trend_price_div.find('p').text.strip()
+        lower_price = lower_price_span.find('span', {'class': 'font-bold'}).text.strip()
 
-product = '농구공'
-alpha = round(obj.score(img)/100, 3)
+        trend_price = re.findall(r'\d+', trend_price)
+        lower_price = re.findall(r'\d+', lower_price)
 
-# 웹 페이지의 URL 설정
-url = f'https://web.joongna.com/search-price/{product}?'
+        lower_price = int(''.join(lower_price))
+        trend_price = int(''.join(trend_price))
+        trend_price = min(int(round(trend_price, -1)), int(1.5*lower_price))
+        return trend_price, lower_price
+    except:
+        return False
+    
+def get_rec_price(upper, lower, alpha) -> int:
+    recommend_price = (1-alpha) * upper + alpha * lower
+    return int(round(recommend_price, -1))
 
-response = requests.post(url)
+def get_results_list(upper, recommend, lower) -> list:
+    results = set([upper, recommend, lower])
+    results = sorted(list(results))
+    return list(results)
 
-# 응답 받은 HTML을 파싱
-soup = BeautifulSoup(response.text, 'html.parser')
+    
+if __name__ == '__main__':
+    path = './original-scaled-image.jpg'
+    img = PIL.Image.open(path)
 
-mean_price_div = soup.find('div', class_='graphTitle')
-lower_price_span = soup.find('span', {'class': 'text-blue-500'})
-
-mean_price = mean_price_div.find('p').text.strip()
-lower_price = lower_price_span.find('span', {'class': 'font-bold'}).text.strip()
-
-mean_price = re.findall(r'\d+', mean_price)
-lower_price = re.findall(r'\d+', lower_price)
-
-mean_price = int(''.join(mean_price))
-lower_price = int(''.join(lower_price))
-
-# price algorithm - will be modified
-result = (1-alpha) * mean_price + alpha * lower_price
-result = int(round(result, -1))
-
-print(result)
+    product = input()
+    alpha = get_brisque_score(img)
+    trend_price, lower_price = scrapp_joogna(product)
+    recommend_price = get_rec_price(trend_price, lower_price, alpha)
+    results = get_results_list(trend_price, recommend_price, lower_price)
+    print(results)
